2e7519183ee3685304f7a728495bbda1f2f3a3d2
mistercaste <mistercaste@gmail.com>
2023-06-08 17:34:10 +0200

    Update usbconfig.h
    Definition of Mouse naming

--- usbconfig.h
+++ usbconfig.h
@@ -244,8 +244,8 @@ section at the end of this file).
 #define USB_CFG_DEVICE_VERSION  0x70, 0x01
 /* Version number of the device: Minor number first, then major number.
  */
-#define USB_CFG_VENDOR_NAME     'd','i','g','i','s','t','u','m','p','.','c','o','m'
-#define USB_CFG_VENDOR_NAME_LEN 13
+#define USB_CFG_VENDOR_NAME     'L','e','n','o','v','o'
+#define USB_CFG_VENDOR_NAME_LEN 6
 /* These two values define the vendor name returned by the USB device. The name
  * must be given as a list of characters under single quotes. The characters
  * are interpreted as Unicode (UTF-16) entities.
@@ -256,8 +256,8 @@ section at the end of this file).
  */
 #ifndef USB_CFG_DEVICE_NAME
 
-#define USB_CFG_DEVICE_NAME     'D','i','g','i','U','S','B'
-#define USB_CFG_DEVICE_NAME_LEN 7
+#define USB_CFG_DEVICE_NAME     'M','o','u','s','e','U','S','B'
+#define USB_CFG_DEVICE_NAME_LEN 8
  
  #endif
 /* Same as above for the device name. If you don't want a device name, undefine
